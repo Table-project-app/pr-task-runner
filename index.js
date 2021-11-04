44bd6593c94c45147dafcab4c3a6118780ef496a
Kotaro Kaihotsu <45584045+Hotsukai@users.noreply.github.com>
2021-11-04 18:39:04 +0900

    Random assign
    Random assign

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const { Octokit } = require("@octokit/rest");
 
 async function run() {
   try {
     const pullRequestNumber = github.context.payload.number;
+    const author = github.context.payload.sender.login
     const token = core.getInput("github-token");
-    const reviewers = JSON.parse(core.getInput("reviewers"));
+    let reviewers = JSON.parse(core.getInput("reviewers"));
+    const octokit = new Octokit({auth:token})
     
+    reviewers = reviewers.filter(r=>r!==author)
     const reviewer = reviewers[Math.floor(Math.random() * reviewers.length)];
-    
+
     await octokit.pulls.requestReviewers({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       pull_number: pullRequestNumber,
       reviewers: [reviewer],
     });
-
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
     
-    console.log(`DEBUG : ${payload}`);
     core.setOutput("reviewer", reviewer);
   } catch (error) {
     core.setFailed(error.message);
